ee76be15effa565708023ecb64670c51dd2be2b1
Ramazan Sancar <ramazansancar4545@gmail.com>
2023-01-09 12:25:41 +0000

    aroundTriangle added.

--- packages/types/point.ts
+++ packages/types/point.ts
@@ -7,4 +7,10 @@ export const around4 = [
   { x: 0, y: 1 },
 ] as const;
 
+export const aroundTriangle = [
+  { x: 1, y: 0 },
+  { x: -1, y: -1 },
+  { x: -1, y: 1 },
+] as const;
+
 export const pointEquals = (a: Point, b: Point) => a.x === b.x && a.y === b.y;
